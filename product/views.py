3945987ff18018f08ab672fc0b5be0bb72bbd028
Habibur Rahaman <habibur@quantummethod.org.bd>
2025-03-12 13:15:38 +0600

    Module 20.8 progress

--- product/views.py
+++ product/views.py
@@ -5,24 +5,36 @@ from rest_framework.response import Response
 from rest_framework import status
 from product.models import Product, Category
 from product.serializers import ProductSerializer, CategorySerializer
+from django.db.models import Count
 
 # Create your views here.
 
-@api_view()
+@api_view(['GET', 'POST'])
 def view_products(request):
-    products = Product.objects.select_related('category').all()
-    serializer = ProductSerializer(products, many = True, context = {'request': request})
-    return Response(serializer.data)
+    if request.method == 'GET':
+        products = Product.objects.select_related('category').all()
+        serializer = ProductSerializer(products, many = True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = ProductSerializer(data=request.data) #Deserializer
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def view_specific_product(request, id):
-    product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        product = get_object_or_404(Product, pk=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
 
 @api_view()
 def view_categories(request):
-    return Response({"messages":"Category"})
+    categories = Category.objects.annotate(
+        product_count = Count('products')).all()
+    serializer = CategorySerializer(categories, many = True)
+    return Response(serializer.data)
 
 @api_view()
 def view_specific_category(request, pk):
