5839d055a5369b8343541e2f18b3776cf30a15d9
Habibur Rahaman <habibur@quantummethod.org.bd>
2025-03-28 16:52:30 +0600

    Module-22 completed

--- order/views.py
+++ order/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from order.models import Cart, CartItem
-from order.serializers import CartSerializer,CartItemSerializer
+from order.serializers import CartSerializer,CartItemSerializer, AddCartItemSerializer, UpdateCartItemSerializer
 # Create your views here.
 
 class CartViewSet(CreateModelMixin, RetrieveModelMixin,DestroyModelMixin, GenericViewSet):
@@ -10,8 +10,16 @@ class CartViewSet(CreateModelMixin, RetrieveModelMixin,DestroyModelMixin, Generi
     serializer_class = CartSerializer
 
 class CartItemViewSet(ModelViewSet):
-    queryset = CartItem.objects.all()
-    serializer_class = CartItemSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddCartItemSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateCartItemSerializer
+        return CartItemSerializer
+
+    def get_serializer_context(self):
+        return {'cart_id': self.kwargs['cart_pk']}
 
     def get_queryset(self):
         return CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])
