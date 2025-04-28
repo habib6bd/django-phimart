77fa1780656adcad77474d68f04fc48f86b89884
Habibur Rahaman <habibur@quantummethod.org.bd>
2025-04-28 13:25:41 +0600

    Module 23.4 completed jwt

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
 import rest_framework
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
     'rest_framework',
+    'djoser',
     'api',
     'users',
     'product',
@@ -141,6 +142,19 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING' : False,
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    # 'PAGE_SIZE': 10
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+   "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+
+}
+DJOSER = {
+    'SERIALIZERS' : {
+        'user_create' : 'users.serializers.UserCreateSerializer',
+        'current_user' : 'users.serializers.UserSerializer',
+    },
 }
